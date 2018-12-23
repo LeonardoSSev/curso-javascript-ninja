bd091c6e961860b3ce30c34c62c6b2dae24f6091
Leonardo Santos <leonardossev@gmail.com>
2018-12-23 01:34:11 -0200

    Completes the first challenge

---
+++ challenge-01/index.js
@@ -0,0 +1,32 @@
+let myvar;
+
+myvar = 10;
+
+let soma = 15 + 8;
+
+soma += 1;
+
+soma *= 3;
+
+console.log(soma)
+
+let souninja = true;
+
+let comida = ['arroz', 'feijão', 'ovo'];
+
+console.log(comida[1]);
+
+if (soma === myvar) {
+
+}
+
+if (myvar < soma) {
+
+}
+
+
+function divisao(number1, number2) {
+    return number1 / number2;
+}
+
+divisao(10, 2);
